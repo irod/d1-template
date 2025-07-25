0679e1d09eef431d5b281990895b0a1cc8c11231
irod <irod@qq.com>
2025-07-26 03:20:01 +0800

    hono

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import { renderHtml } from "./renderHtml";
+
+// export default {
+//   async fetch(request, env) {
+//     const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
+//     const { results } = await stmt.all();
+
+//     return new Response(renderHtml(JSON.stringify(results, null, 2)), {
+//       headers: {
+//         "content-type": "text/html",
+//       },
+//     });
+//   },
+// } satisfies ExportedHandler<Env>;
+
+
+import { Hono } from 'hono'
+// import { renderer } from './renderer'
+
+const app = new Hono()
+
+// app.use(renderer)
+
+app.get('/', async (c) => {
+  const stmt = c.env.DB.prepare("SELECT * FROM comments LIMIT 3");
+    const { results } = await stmt.all();
+
+    return new Response(renderHtml(JSON.stringify(results, null, 2)), {
+      headers: {
+        "content-type": "text/html",
+      },
+    });
+  return c.text('Hello Cloudflare Workers irod!')
+})
+
+export default app
