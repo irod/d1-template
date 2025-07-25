824f2e9ee32ee529bb8fbebb0fdeca1826e19a73
irod <irod@qq.com>
2025-07-26 03:34:00 +0800

    hono text

--- src/index.js
+++ src/index.js
@@ -22,15 +22,15 @@ const app = new Hono()
 // app.use(renderer)
 
 app.get('/', async (c) => {
-  const stmt = c.env.DB.prepare("SELECT * FROM comments LIMIT 3");
+  const stmt = c.env.DB.prepare("SELECT * FROM comments LIMIT 1");
     const { results } = await stmt.all();
 
-    return new Response(renderHtml(JSON.stringify(results, null, 2)), {
-      headers: {
-        "content-type": "text/html",
-      },
-    });
-  return c.text('Hello Cloudflare Workers irod!')
+    // return new Response(renderHtml(JSON.stringify(results, null, 2)), {
+    //   headers: {
+    //     "content-type": "text/html",
+    //   },
+    // });
+  return c.text('Hello Cloudflare Workers irod!'+JSON.stringify(results, null, 2))
 })
 
 export default app
